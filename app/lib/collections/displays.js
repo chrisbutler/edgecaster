33965adb7472c264a3dbcb1ee13ab56e9cf29f23
Chris Butler <chris@platformthirteen.com>
2015-03-09 00:27:20 -0400

    init

---
+++ app/lib/collections/displays.js
@@ -0,0 +1,32 @@
+Displays = new Mongo.Collection('displays');
+
+
+if (Meteor.isServer) {
+  Displays.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Displays.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
