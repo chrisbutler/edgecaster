84a128a0c08841ff8f6dc76ee07b027b499a1b57
Chris Butler <chris@platformthirteen.com>
2015-08-04 14:45:42 -0400

    type.js working

--- app/client/templates/screen/screen.js
+++ app/client/templates/screen/screen.js
@@ -8,33 +8,43 @@ Template.Screen.rendered = function() {
   console.log('Screen rendered', this);
 }
 
-Template._full.rendered = function() {
-  console.log('_full rendered');
-  if (this.question) {
-    var s = [];
-    var e = $("#comment-text");
-    s.push(this.question.comment);
+Template._full.onRendered(function () {
+  console.log('full rendered', this);
+  this.autorun(function () {
+    var d = Displays.findOne({_id: Router.current().params._id});
+    var q = Questions.findOne({_id: d.question})
+    if (d && q) {
+      $("#comment-text").css({ 'opacity': 0}).append('<span></span>');
+      $("#comment-text span").text(q.comment);
 
-    var f = (s[0].length / 100) + .25;
-    e.fitText(f, {
-      minFontSize: '32px',
-      maxFontSize: '72px'
-    })
+      var f = q.comment.length / 100;
+      console.log('text', q.comment.length, f);
 
-    if (e.data('typed')) {
-      //e.typed('reset');
-      e.removeData('typed')
-      $('.typed-cursor').remove();
-    }
 
-    e.typed({
-      strings: s,
-      resetCallback: function() {
-        console.log('reset', this);
-      },
-      // callback: function() {
-      //   console.log('typed callback', this);
+      $("#comment-text").fitText(f, {
+        minFontSize: '54px',
+        maxFontSize: '108px'
+      })
+
+
+
+      // if ($("#comment-text span").data('typed')) {
+      //   $("#comment-text span").typed('reset');
+      //   // e.removeData('typed')
+      //   // $('.typed-cursor').remove();
       // }
-    });
-  }
-}
+
+      $("#comment-text").empty().css({'opacity': 1}).append('<p></p>');
+      $("#comment-text p").typed({
+        strings: [q.comment],
+        contentType: 'text',
+        resetCallback: function() {
+          console.log('reset', this);
+        },
+        // callback: function() {
+        //   console.log('typed callback', this);
+        // }
+      });
+    }
+  })
+});
